0305f716f6701d62bb3c79066a6202a8a965763c
edmar <edsiqueira73@gmail.com>
2021-06-18 14:18:22 -0300

    chore: add event maker sign up function at repo

--- src/modules/event-makers/repositories/event-maker.repository.ts
+++ src/modules/event-makers/repositories/event-maker.repository.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from '@modules/prisma'
+import { Prisma, PrismaService } from '@modules/prisma'
+import { EventMaker } from '@common/models/event-maker.model'
 
 @Injectable()
 export class EventMakerRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async create(input: Prisma.EventMakerCreateInput): Promise<EventMaker> {
+    const user = await this.prismaService.eventMaker.create({ data: input })
+    return user as EventMaker
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.prismaService.eventMaker.findUnique({ where: { email } })
+    return user
+  }
+
+  async findByCnpj(cnpj: string) {
+    const user = await this.prismaService.eventMaker.findUnique({ where: { cnpj } })
+    return user
+  }
 }
