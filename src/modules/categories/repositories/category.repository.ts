c96dbebd1fb5317658d3c85d4d65b56516a2d50b
edmar <edsiqueira73@gmail.com>
2021-06-19 16:36:41 -0300

    chore: add category repository

--- src/modules/categories/repositories/category.repository.ts
+++ src/modules/categories/repositories/category.repository.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '@modules/prisma'
+import { CreateCategoryDto } from '@common/dtos'
+import { Category } from '@common/models'
 
 @Injectable()
 export class CategoryRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createCategory(input: CreateCategoryDto): Promise<Category> {
+    const category = await this.prismaService.category.create({ data: input })
+    return category as Category
+  }
+
+  async getCategories(): Promise<Category[]> {
+    const categories = await this.prismaService.category.findMany()
+    return categories
+  }
 }
