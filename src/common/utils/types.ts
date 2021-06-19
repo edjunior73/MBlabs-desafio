29416b641a5c7f6ab37f513841ac637a78bc57a5
edmar <edsiqueira73@gmail.com>
2021-06-19 16:30:07 -0300

    feat: add util ts types

--- src/common/utils/types.ts
+++ src/common/utils/types.ts
@@ -2,3 +2,8 @@
 export const isFunction = (value: any): value is Function => {
   return typeof value === 'function'
 }
+
+export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Omit<T, Keys> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Omit<T, Keys>>
+  }[Keys]
