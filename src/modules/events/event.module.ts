1540dc8bc8661489805b4323b0ed1b69dae01828
edmar <edsiqueira73@gmail.com>
2021-06-20 10:42:41 -0300

    chore: add ticket and events providers

--- src/modules/events/event.module.ts
+++ src/modules/events/event.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
-import { EventRepository } from './repositories/event.repository'
-import { TicketRepository } from './repositories/ticket.repository'
+import { EventMakerModule } from '@modules/event-makers'
+import { CategoryModule } from '@modules/categories'
+import { CommandHandlers, QueryHandlers } from './cqrs'
+import { EventRepository, TicketRepository } from './repositories'
+import { EventResolver, TicketResolver } from './resolvers'
+import { EventService, TicketService } from './services'
 
 @Module({
-  imports: [CqrsModule],
-  providers: [EventRepository, TicketRepository]
+  imports: [CqrsModule, EventMakerModule, CategoryModule],
+  providers: [
+    EventRepository,
+    EventService,
+    EventResolver,
+    TicketResolver,
+    TicketRepository,
+    TicketService,
+    ...CommandHandlers,
+    ...QueryHandlers
+  ]
 })
 export class EventModule {}
