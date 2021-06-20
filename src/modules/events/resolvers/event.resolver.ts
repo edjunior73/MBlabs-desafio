51ae1e1a306b8dce4c98da2cf2681d25c4312c3c
edmar <edsiqueira73@gmail.com>
2021-06-20 10:36:58 -0300

    chore: add event resolver

---
+++ src/modules/events/resolvers/event.resolver.ts
@@ -0,0 +1,28 @@
+import { UseGuards } from '@nestjs/common'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { CreateEventDto } from '@common/dtos'
+import { Event } from '@common/models'
+import { Roles, AuthUser } from '@common/decorators'
+import { AuthGuard } from '@common/guards'
+import { Role, JUser } from '@common/types'
+import { EventService } from '../services/event.service'
+
+@Resolver()
+export class EventResolver {
+  constructor(private readonly eventService: EventService) {}
+
+  @Query(() => [Event])
+  getEvents() {
+    return this.eventService.getEvents()
+  }
+
+  @Mutation(() => Event)
+  @UseGuards(AuthGuard)
+  @Roles(Role.EVENT_MAKER)
+  createEvent(@Args('createEventInput') input: CreateEventDto, @AuthUser() user: JUser) {
+    return this.eventService.createEvent({
+      ...input,
+      ownerId: user.id
+    })
+  }
+}
