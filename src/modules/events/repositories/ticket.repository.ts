bcd7170304107d828d319d10fc3b33119e0a79d7
edmar <edsiqueira73@gmail.com>
2021-06-20 10:40:31 -0300

    feat: add ticket repository

--- src/modules/events/repositories/ticket.repository.ts
+++ src/modules/events/repositories/ticket.repository.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from '@modules/prisma'
+import { Prisma, PrismaService, Ticket as PrismaTicket } from '@modules/prisma'
+import { Ticket } from '@common/models'
+import { getNonNullKeys } from '@common/utils'
 
 @Injectable()
 export class TicketRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createTicket(input: Prisma.TicketUncheckedCreateInput): Promise<Ticket> {
+    const ticket = await this.prismaService.ticket.create({ data: input })
+    return this.formatTicket(ticket) as Ticket
+  }
+
+  async findByEventAndName(eventId: string, name: string): Promise<Ticket | null> {
+    const ticket = await this.prismaService.ticket.findFirst({ where: { eventId, name } })
+    return this.formatTicket(ticket)
+  }
+
+  private formatTicket(ticket: PrismaTicket | null): Ticket | null {
+    if (ticket) {
+      return getNonNullKeys(ticket)
+    }
+    return null
+  }
 }
