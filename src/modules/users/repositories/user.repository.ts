d6187fecdd339a22e2a5d1524c9ba7b38724d6ac
edmar <edsiqueira73@gmail.com>
2021-06-19 21:17:47 -0300

    chore: att user repository

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { Prisma, PrismaService } from '@modules/prisma'
-import { User } from '@common/models/user.model'
+import { Prisma, PrismaService, User as PrismaUser } from '@modules/prisma'
+import { User } from '@common/models'
+import { UpdateUserDto } from '@common/dtos'
+import { getNonNullKeys } from '@common/utils'
 
 @Injectable()
 export class UserRepository {
@@ -8,20 +10,52 @@ export class UserRepository {
 
   async create(input: Prisma.UserCreateInput): Promise<User> {
     const user = await this.prismaService.user.create({ data: input })
-    return user as User
+    return this.formatUser(user) as User
   }
 
-  async findByEmail(email: string) {
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prismaService.user.findUnique({ where: { id } })
+    return this.formatUser(user)
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
     const user = await this.prismaService.user.findUnique({ where: { email } })
-    return user
+    return this.formatUser(user)
   }
 
-  async findByCpf(cpf: string) {
+  async findByCpf(cpf: string): Promise<User | null> {
     const user = await this.prismaService.user.findUnique({ where: { cpf } })
-    return user
+    return this.formatUser(user)
   }
-  async getUser(email: string): Promise<User> {
-    const user = await this.prismaService.user.findUnique({ where: { email } })
-    return user as User
+
+  async existsById(id: string): Promise<boolean> {
+    const count = await this.prismaService.user.count({
+      where: {
+        id
+      }
+    })
+    return count > 0
+  }
+
+  async updateUser(input: Prisma.UserUpdateInput, userId: string): Promise<User> {
+    const updatedUser = await this.prismaService.user.update({
+      data: input,
+      where: {
+        id: userId
+      }
+    })
+    return this.formatUser(updatedUser) as User
+  }
+
+  async deleteUser(userId: string): Promise<boolean> {
+    const deletedUser = await this.prismaService.user.delete({ where: { id: userId } })
+    return !!deletedUser
+  }
+
+  private formatUser(user: PrismaUser | null): User | null {
+    if (user !== null) {
+      return getNonNullKeys(user)
+    }
+    return null
   }
 }
