22786beb0d9d8d2446847a3315ee360ef6de734e
edmar <edsiqueira73@gmail.com>
2021-06-19 21:00:40 -0300

    chore: att event maker resolver

---
+++ src/modules/event-makers/resolvers/event-maker.resolver.ts
@@ -0,0 +1,25 @@
+import { SignUpEventMakerDto, LoginDto } from '@common/dtos'
+import { EventMaker } from '@common/models'
+import { EventMakerLogin } from '@common/models/event-maker-login.model'
+import { Resolver, Mutation, Args, Query, ID } from '@nestjs/graphql'
+import { EventMakerService } from '../services/event-maker.service'
+
+@Resolver()
+export class EventMakerResolver {
+  constructor(private readonly eventMakerService: EventMakerService) {}
+
+  @Query(() => EventMaker, { name: 'eventMakerById', nullable: true })
+  getEventMakerById(@Args('eventMakerId', { type: () => ID }) id: string) {
+    return this.eventMakerService.getEventMakerById(id)
+  }
+
+  @Mutation(() => EventMakerLogin)
+  async signUpEventMaker(@Args('eventMakerInput') input: SignUpEventMakerDto) {
+    return this.eventMakerService.signUpEventMaker(input)
+  }
+
+  @Mutation(() => EventMakerLogin)
+  async loginEventMaker(@Args('eventMakerInput') input: LoginDto) {
+    return this.eventMakerService.loginEventMaker(input)
+  }
+}
