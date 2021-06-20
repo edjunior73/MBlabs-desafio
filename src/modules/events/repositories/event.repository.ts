dc488e62cb94cb1261cc41f8dc6bd8ca84fccd19
edmar <edsiqueira73@gmail.com>
2021-06-20 10:36:27 -0300

    feat: event repository

--- src/modules/events/repositories/event.repository.ts
+++ src/modules/events/repositories/event.repository.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '@modules/prisma'
 
+import { CreateEventDto } from '@common/dtos'
+import { Event } from '@common/models'
+
 @Injectable()
 export class EventRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async create(input: CreateEventDto): Promise<Event> {
+    const event = await this.prismaService.event.create({ data: input })
+    return event
+  }
+
+  async findByName(name: string): Promise<Event | null> {
+    const event = await this.prismaService.event.findUnique({ where: { name } })
+    return event
+  }
+
+  async findById(id: string): Promise<Event | null> {
+    const event = await this.prismaService.event.findUnique({ where: { id } })
+    return event
+  }
+
+  async getEvents(): Promise<Event[]> {
+    const events = await this.prismaService.event.findMany()
+    return events
+  }
 }
