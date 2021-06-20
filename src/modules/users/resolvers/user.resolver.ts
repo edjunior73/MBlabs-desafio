0b31eaba87c6941d8096f97aeab6783b2ba0a539
edmar <edsiqueira73@gmail.com>
2021-06-19 21:18:05 -0300

    chore: att user resolver

--- src/modules/users/resolvers/user.resolver.ts
+++ src/modules/users/resolvers/user.resolver.ts
@@ -1,17 +1,53 @@
-import { GetUserDto, SignUpUserDto } from '@common/dtos'
-import { UserLogin } from '@common/models/user-login.model'
-import { UserService } from '@modules/users/services'
+import { UseGuards } from '@nestjs/common'
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
+import { isEmail } from 'class-validator'
+import { SignUpUserDto, UpdateUserDto, LoginDto } from '@common/dtos'
+import { User, UserLogin } from '@common/models'
+import { Roles, AuthUser } from '@common/decorators'
+import { AuthGuard } from '@common/guards'
+import { Role, JUser } from '@common/types'
+import { UserService } from '@modules/users/services'
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
-  @Query(() => String)
-  getUser(@Args('userInput') input: GetUserDto) {
-    return this.userService.getUser(input)
+
+  @Query(() => User, { nullable: true, name: 'userByEmail' })
+  getUserByEmail(@Args('email') email: string) {
+    if (!isEmail(email)) {
+      throw new Error('Precisa ser inserido um email válido')
+    }
+    return this.userService.getUserByEmail(email)
   }
+
+  @Query(() => User)
+  @UseGuards(AuthGuard)
+  @Roles(Role.USER)
+  me(@AuthUser() user: JUser) {
+    return this.userService.getUserByEmail(user.email)
+  }
+
   @Mutation(() => UserLogin)
   signUpUser(@Args('userInput') input: SignUpUserDto) {
     return this.userService.signUpUser(input)
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(AuthGuard)
+  @Roles(Role.USER)
+  deleteUser(@AuthUser() user: JUser) {
+    return this.userService.deleteUser(user.id)
+  }
+
+  @Mutation(() => UserLogin)
+  loginUser(@Args('userInput') input: LoginDto) {
+    return this.userService.loginUser(input)
+  }
+
+  @Mutation(() => User)
+  @UseGuards(AuthGuard)
+  @Roles(Role.USER)
+  updateUser(@Args('userInput') input: UpdateUserDto, @AuthUser() user: JUser) {
+    return this.userService.updateUser(input, user.id)
   }
 }
