df61cedf2865507bb5ef1e9c8edcbe732a828f6c
edmar <edsiqueira73@gmail.com>
2021-06-20 10:37:57 -0300

    chore: add event service

---
+++ src/modules/events/services/event.service.ts
@@ -0,0 +1,22 @@
+import { CreateEventDto } from '@common/dtos'
+import { Injectable } from '@nestjs/common'
+import { CommandBus, EventBus, QueryBus } from '@nestjs/cqrs'
+import { Event } from '@prisma/client'
+import { CreateEventCommand } from '../cqrs'
+import { GetEventsQuery } from '../cqrs/queries/get-events/get-events.query'
+
+@Injectable()
+export class EventService {
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+    private readonly eventBus: EventBus
+  ) {}
+
+  getEvents(): Promise<Event[]> {
+    return this.queryBus.execute(new GetEventsQuery())
+  }
+  createEvent(input: CreateEventDto): Promise<Event> {
+    return this.commandBus.execute(new CreateEventCommand(input))
+  }
+}
