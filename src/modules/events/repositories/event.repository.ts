4918d47b2e05c5800c53ce9c69a16befc3afc5b4
edmar <edsiqueira73@gmail.com>
2021-06-22 14:47:59 -0300

    chore: att event repository

--- src/modules/events/repositories/event.repository.ts
+++ src/modules/events/repositories/event.repository.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from '@modules/prisma'
-
-import { CreateEventDto } from '@common/dtos'
-import { Event } from '@common/models'
+import { Prisma, PrismaService } from '@modules/prisma'
+import { CreateEventDto, UpdateEventDto } from '@common/dtos'
+import { Event, PaginatedEvents } from '@common/models'
+import { BaseRepository } from '@common/classes/base-repository.class'
+import { EventPaginationParams } from '../types'
 
 @Injectable()
-export class EventRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+export class EventRepository extends BaseRepository<Event> {
+  constructor(private readonly prismaService: PrismaService) {
+    super()
+  }
 
   async create(input: CreateEventDto): Promise<Event> {
     if (input.date.getTime() < Date.now())
       throw new Error('Você não pode criar eventos no passado!')
     const event = await this.prismaService.event.create({ data: input })
+    return event
+  }
+
+  async getEventAnalytics(eventId: string, eventMakerId: string) {
+    const event = await this.prismaService.event.findMany({
+      where: { id: eventId, ownerId: eventMakerId }
+    })
     return event
   }
 
@@ -30,8 +40,71 @@ export class EventRepository {
     return events
   }
 
+  async getPaginatedEvents(params: EventPaginationParams = {}): Promise<PaginatedEvents> {
+    const page = this.getPage(params.page)
+    const pageSize = this.getPageSize(params.pageSize)
+
+    const { minPrice, maxPrice, categoryId, name } = params
+
+    const findManyEventArgs: Prisma.EventFindManyArgs = {
+      where: {
+        OR: [
+          minPrice || maxPrice
+            ? {
+                tickets: {
+                  some: {
+                    price: {
+                      lte: maxPrice,
+                      gte: minPrice
+                    }
+                  }
+                }
+              }
+            : null,
+          categoryId
+            ? {
+                categoryId
+              }
+            : null,
+          name ? { name: { contains: name, mode: 'insensitive' } } : null
+        ].filter(Boolean) as Prisma.EventWhereInput
+      },
+      take: pageSize,
+      skip: this.getPaginationOffset({ page, pageSize })
+    }
+
+    if (
+      Array.isArray(findManyEventArgs.where?.OR) &&
+      findManyEventArgs.where?.OR.length === 0
+    ) {
+      delete findManyEventArgs.where.OR
+    }
+
+    const count = await this.prismaService.event.count({
+      where: findManyEventArgs.where
+    })
+
+    const events: Event[] = await this.prismaService.event.findMany(findManyEventArgs)
+    return this.createPaginationPayload({
+      data: events,
+      count,
+      page,
+      pageSize
+    })
+  }
+
+  async updateEvent(input: UpdateEventDto, eventId: string): Promise<Event> {
+    const updatedEvent = await this.prismaService.event.update({
+      data: input,
+      where: {
+        id: eventId
+      }
+    })
+    return updatedEvent
+  }
+
   async cancelEvent(eventId: string, eventMakerId: string): Promise<boolean> {
-    let i2 = 0
+    let i = 0
     const event = await this.prismaService.event.findUnique({ where: { id: eventId } })
 
     if (!event) throw new Error('Este evento não existe')
@@ -40,11 +113,11 @@ export class EventRepository {
     const tickets = await this.prismaService.ticket.findMany({
       where: { eventId }
     })
-    while (tickets[i2] !== undefined) {
+    while (tickets[i] !== undefined) {
       const deletedRelations = await this.prismaService.userEvent.deleteMany({
-        where: { ticketId: tickets[i2].id }
+        where: { ticketId: tickets[i].id }
       })
-      i2 += 1
+      i += 1
     }
     const deletedTickets = await this.prismaService.ticket.deleteMany({
       where: { eventId }
