35cc1f5d96869a2823d8b599ecdc3b2f4b730f8b
edmar <edsiqueira73@gmail.com>
2021-06-22 14:51:21 -0300

    chore: add update event service

--- src/modules/events/services/event.service.ts
+++ src/modules/events/services/event.service.ts
@@ -1,9 +1,16 @@
-import { CreateEventDto } from '@common/dtos'
+import { CreateEventDto, SearchEventsArgs, UpdateEventDto } from '@common/dtos'
+import { PaginatedEvents } from '@common/models'
 import { Injectable } from '@nestjs/common'
 import { CommandBus, EventBus, QueryBus } from '@nestjs/cqrs'
 import { Event } from '@prisma/client'
-import { CancelEventCommand, CreateEventCommand } from '../cqrs'
-import { GetEventsQuery } from '../cqrs/queries/get-events/get-events.query'
+import {
+  CancelEventCommand,
+  CreateEventCommand,
+  GetEventsQuery,
+  SearchEventsQuery
+} from '../cqrs'
+import { UpdateEventCommand } from '../cqrs/commands/update-event/update-event.command'
+import { GetEventAnalyticsQuery } from '../cqrs/queries/get-event-analytics'
 
 @Injectable()
 export class EventService {
@@ -16,10 +23,23 @@ export class EventService {
   getEvents(): Promise<Event[]> {
     return this.queryBus.execute(new GetEventsQuery())
   }
+
+  getPaginatedEvents(args: SearchEventsArgs): Promise<PaginatedEvents> {
+    return this.queryBus.execute(
+      new SearchEventsQuery(args.filter, { page: args.page, pageSize: args.pageSize })
+    )
+  }
+
   createEvent(input: CreateEventDto): Promise<Event> {
     return this.commandBus.execute(new CreateEventCommand(input))
   }
+  updateEvent(input: UpdateEventDto, eventId: string): Promise<Event> {
+    return this.commandBus.execute(new UpdateEventCommand(input, eventId))
+  }
   cancelEvent(eventId: string, eventMakerId: string) {
     return this.commandBus.execute(new CancelEventCommand(eventId, eventMakerId))
+  }
+  getEventAnalytics(eventId: string, eventMakerId: string) {
+    return this.queryBus.execute(new GetEventAnalyticsQuery(eventId, eventMakerId))
   }
 }
